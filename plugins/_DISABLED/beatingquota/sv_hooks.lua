7e5019071c4381005ed6fb1641b8b3c3f06bd6d9
Amandq <134674497+Amandq@users.noreply.github.com>
2023-05-27 04:09:58 -0500

    I made it work so now I urm good!

--- plugins/_DISABLED/beatingquota/sv_hooks.lua
+++ plugins/_DISABLED/beatingquota/sv_hooks.lua
@@ -3,12 +3,12 @@ local PLUGIN = PLUGIN
 function PLUGIN:PlayerLoadedCharacter(client, character)
     if client:Team() == FACTION_CCA then
         if character:GetData("quota") == nil then
-            character:SetData("quota", 99)
+            character:SetData("quota", 0)
             print(client:Nick() .. " has no quota data, setting quota data for them.")
         end
 
         if character:GetData("quotamax") == nil then
-            character:SetData("quotamax", 99)
+            character:SetData("quotamax", 3)
             print(client:Nick() .. " has no (MAX) quota data, setting (MAX) quota data for them.")
         end
     end
@@ -18,40 +18,47 @@ function PLUGIN:EntityTakeDamage(target, dmginfo)
     if not target:IsPlayer() then return end
     local attacker = dmginfo:GetAttacker()
     if not attacker:IsPlayer() or attacker:Team() ~= FACTION_CCA then return end
+
     local quotaamount = attacker:GetData("quota") or 0
-    local quotamax = attacker:GetData("quotamax") or 0
-    if quotaamount >= 99 or quotamax >= 99 then return end
+    local quotamax = attacker:GetData("quotamax") or 3
+
+    if quotaamount >= quotamax then return end
 
     if target:Team() == FACTION_CITIZEN and (attacker:GetActiveWeapon():GetClass() == "ix_hands" or attacker:GetActiveWeapon():GetClass() == "ix_stunstick") then
-        if quotaamount < quotamax then
-            attacker:SetData("quota", quotaamount + 1)
+        attacker:SetData("quota", quotaamount + 1)
+
+        if quotaamount + 1 >= quotamax then
+            attacker:Notify("You have completed your Beating Quota!")
+            local character = attacker:GetCharacter()
+            character:SetData("quota", 0)
+            character:SetData("quotamax", 3)
+            character:GiveMoney(5)
+            attacker:Notify("Your Beating Quota has been reset. You have been rewarded $5.")
+
+            timer.Create("ResetQuotaTimer", 60, 1, function()
+                attacker:SetData("quota", 0)
+                attacker:Notify("Your Beating Quota has been reset due to inactivity.")
+            end)
+        else
             attacker:Notify("You have gained a point on your Beating Quota, you are now at (" .. (quotaamount + 1) .. "/" .. quotamax .. ")")
-            --We query Quota Data again, as opposed to using "quotaamount" because the tick does not update fast enough for the print, resulting in the first hit showing at 0/3
         end
     end
 end
 
-function PLUGIN:PlayerTick(ply)
-    for k, v in pairs(player.GetAll()) do
-        if v:IsCombine() then
-            local quotaamount = v:GetData("quota") or 0
-            local quotamax = v:GetData("quotamax") or 0
-            local character = v:GetCharacter()
+local function ResetQuota(client)
+    local character = client:GetCharacter()
+    character:SetData("quota", 0)
+    character:SetData("quotamax", 3)
+end
 
-            if quotaamount >= quotamax and quotamax ~= 99 and quotaamount ~= 99 then
-                character:GetData("quota", 0) -- set the quota to 0
-                character:GetData("quotamax", 99)
-                character:GiveMoney(5)
-                v:Notify("You've met your Beating Quota, you'll be assigned a new one when the time comes.")
-            end
+ix.command.Add("ResetQuota", {
+    description = "Reset your beating quota.",
+    OnRun = function(self, client)
+        if client:Team() == FACTION_CCA then
+            ResetQuota(client)
+            client:Notify("Your Beating Quota has been reset.")
+        else
+            client:Notify("You need to be a CCA to use this command.")
         end
     end
-end
-local function ResetQuotaTimer()
-    for _, ply in ipairs(player.GetAll()) do
-        ply:SetData("quota", 0)
-        ply:SetData("quotamax", math.random(1, 9))
-    end
-end
-
-timer.Create("QuotaResetTimer", 600, 0, ResetQuotaTimer) -- Timer resets every 10 minutes (600 seconds)
+})  
