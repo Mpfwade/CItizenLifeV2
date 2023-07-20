1cd0724361525c14a63ca12327d4fe425709ee0c
Mpfwade <wrightderek036@gmail.com>
2023-07-20 19:25:01 -0400

    KYS

--- plugins/curfew/sh_plugin.lua
+++ plugins/curfew/sh_plugin.lua
@@ -2,7 +2,6 @@ local PLUGIN = PLUGIN
 PLUGIN.name = "Random Inspections and Rations"
 PLUGIN.author = "OG Maker: Scotnay (Edited by Wade)"
 PLUGIN.description = "Random events"
-
 PLUGIN.events = {
     {
         name = "INSPECTION",
@@ -29,12 +28,9 @@ PLUGIN.events = {
 PLUGIN.nextRationEvent = nil
 PLUGIN.nextInspectionEvent = nil
 PLUGIN.inspectionCounter = 0
-
 SetGlobalBool("ixRationOnline", false)
-
 function PLUGIN:GetEvent()
     local curTime = CurTime()
-
     if not self.nextRationEvent then
         self.nextRationEvent = curTime + self.events[2].cooldown
     end
@@ -45,22 +41,22 @@ function PLUGIN:GetEvent()
 
     local timeSinceRationEvent = curTime - self.nextRationEvent
     local timeSinceInspectionEvent = curTime - self.nextInspectionEvent
-
     if timeSinceInspectionEvent >= self.events[1].cooldown then
         if not self:IsEventActive() and ix.config.Get("cityCode") < 1 and self:GetNextEvent() == "Inspection" then
             local inspectionEvent = self.events[1]
             inspectionEvent.start(true)
             SetGlobalString("ixCurrentEvent", inspectionEvent.name)
             self.inspectionCounter = self.inspectionCounter + 1
-
-            timer.Create("InspectionTimer", inspectionEvent.duration, 1, function()
-                inspectionEvent.start(false)
-                self.nextInspectionEvent = curTime + inspectionEvent.cooldown
-
-                if not self:IsEventActive() then
+            timer.Create(
+                "InspectionTimer",
+                inspectionEvent.duration,
+                1,
+                function()
+                    inspectionEvent.start(false)
                     SetGlobalString("ixCurrentEvent", "PATROL, PROTECT")
+                    self.nextInspectionEvent = curTime + inspectionEvent.cooldown
                 end
-            end)
+            )
         end
     end
 
@@ -69,16 +65,17 @@ function PLUGIN:GetEvent()
             local rationEvent = self.events[2]
             rationEvent.start(true)
             SetGlobalString("ixCurrentEvent", rationEvent.name)
-
-            timer.Create("RationTimer", rationEvent.duration, 1, function()
-                rationEvent.start(false)
-                self.nextRationEvent = curTime + rationEvent.cooldown
-                self.inspectionCounter = 0
-
-                if not self:IsEventActive() then
+            timer.Create(
+                "RationTimer",
+                rationEvent.duration,
+                1,
+                function()
+                    rationEvent.start(false)
                     SetGlobalString("ixCurrentEvent", "PATROL, PROTECT")
+                    self.nextRationEvent = curTime + rationEvent.cooldown
+                    self.inspectionCounter = 0
                 end
-            end)
+            )
         end
     end
 
@@ -93,20 +90,11 @@ function PLUGIN:GetEvent()
         end
     end
 
-    local events = {
-        [0] = "PATROL, PROTECT",
-        [1] = "PRESSURE, SWORD, STERILIZE",
-        [2] = "ASSEMBLE, ADMINISTER, CONTAIN",
-        [3] = "FLINT, INSPECT, EXTIRPATE",
-        [4] = "AMPUTATE, ZERO, CONFIRM"
-    }
-
-    local currentEvent = GetGlobalString("ixCurrentEvent", "PATROL, PROTECT")
-    if not self:IsEventActive() then
-        SetGlobalString("ixCurrentEvent", events[ix.config.Get("cityCode")])
+    if ix.config.Get("cityCode") > 0 then
+        SetGlobalString("ixCurrentEvent", "PATROL, PROTECT")
     end
 
-    return currentEvent
+    return GetGlobalString("ixCurrentEvent", "PATROL, PROTECT")
 end
 
 function PLUGIN:Think()
@@ -115,7 +103,6 @@ end
 
 function PLUGIN:IsEventActive()
     local currentEvent = GetGlobalString("ixCurrentEvent", "PATROL, PROTECT")
-
     for _, event in ipairs(self.events) do
         if event.name == currentEvent then return true end
     end
@@ -124,18 +111,20 @@ function PLUGIN:IsEventActive()
 end
 
 function PLUGIN:Initialize()
-    hook.Add("Think", "RandomEventThink", function()
-        self:Think()
-    end)
+    hook.Add(
+        "Think",
+        "RandomEventThink",
+        function()
+            self:Think()
+        end
+    )
 end
-
 
 function PLUGIN:GetNextEvent()
     if SERVER then
         local curTime = CurTime()
         local inspectionCount = self.inspectionCounter
         local number = math.random(1, 3)
-
         if inspectionCount < number then
             if curTime >= self.nextInspectionEvent then
                 return "Inspection"
@@ -155,79 +144,89 @@ function PLUGIN:GetNextEvent()
 end
 
 ix.util.Include("cl_hooks.lua")
-
-ix.command.Add("Closerations", {
-    description = "Close Rations",
-    OnCheckAccess = function(_, ply) return ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() end,
-    OnRun = function(_, ply)
-        SetGlobalBool("ixRationOnline", false)
-        ix.chat.Send(ply, "adminchat", "Rations Closed", true)
-    end
-})
-
-ix.command.Add("Curfew", {
-    description = "Call the Curfew",
-    OnCheckAccess = function(_, ply) return ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() end,
-    OnRun = function(_, ply)
-        SetGlobalBool("ixRationOnline", false)
-        PlayTimedEventSound(1, "npc/overwatch/cityvoice/f_trainstation_assemble_spkr.wav")
-
-        local sounds = {"npc/overwatch/radiovoice/on3.wav", "npc/overwatch/radiovoice/attention.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/search.wav", "npc/overwatch/radiovoice/inprogress.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/preparetoinnoculate.wav", "npc/overwatch/radiovoice/off2.wav"}
-
-        for _, v in ipairs(player.GetAll()) do
-            if v:IsCombine() then
-                ix.util.EmitQueuedSounds(v, sounds, 0, 0.2, 100)
-            end
+ix.command.Add(
+    "Closerations",
+    {
+        description = "Close Rations",
+        OnCheckAccess = function(_, ply) return ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() end,
+        OnRun = function(_, ply)
+            SetGlobalBool("ixRationOnline", false)
+            ix.chat.Send(ply, "adminchat", "Rations Closed", true)
         end
+    }
+)
 
-        ix.chat.Send(ply, "dispatchradio", "Attention, city-wide block search in progress. All local Protection Team units: prepare to inoculate.")
-        ix.chat.Send(ply, "dispatch", "Citizen notice: priority identification check in progress. Please assemble in your designated inspection positions.")
-    end
-})
-
-concommand.Add("ix_curfew", function(ply, cmd, args, client)
-    if ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() then
-        SetGlobalBool("ixRationOnline", false)
-        PlayTimedEventSound(4, "npc/overwatch/cityvoice/f_trainstation_assemble_spkr.wav")
-
-        local sounds = {"npc/overwatch/radiovoice/on3.wav", "npc/overwatch/radiovoice/attention.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/search.wav", "npc/overwatch/radiovoice/inprogress.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/preparetoinnoculate.wav", "npc/overwatch/radiovoice/off2.wav"}
-
-        for _, v in ipairs(player.GetAll()) do
-            if v:IsCombine() then
-                ix.util.EmitQueuedSounds(v, sounds, 0, 0.2, 100)
+ix.command.Add(
+    "Curfew",
+    {
+        description = "Call the Curfew",
+        OnCheckAccess = function(_, ply) return ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() end,
+        OnRun = function(_, ply)
+            SetGlobalBool("ixRationOnline", false)
+            PlayTimedEventSound(1, "npc/overwatch/cityvoice/f_trainstation_assemble_spkr.wav")
+            local sounds = {"npc/overwatch/radiovoice/on3.wav", "npc/overwatch/radiovoice/attention.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/search.wav", "npc/overwatch/radiovoice/inprogress.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/preparetoinnoculate.wav", "npc/overwatch/radiovoice/off2.wav"}
+            for _, v in ipairs(player.GetAll()) do
+                if v:IsCombine() then
+                    ix.util.EmitQueuedSounds(v, sounds, 0, 0.2, 100)
+                end
             end
-        end
 
-        ix.chat.Send(ply, "dispatchradio", "Attention, city-wide block search in progress. All local Protection Team units: prepare to inoculate.")
-        timer.Simple(4, function()
+            ix.chat.Send(ply, "dispatchradio", "Attention, city-wide block search in progress. All local Protection Team units: prepare to inoculate.")
             ix.chat.Send(ply, "dispatch", "Citizen notice: priority identification check in progress. Please assemble in your designated inspection positions.")
-        end)
-    end
-end)
-
-concommand.Add("ix_rations", function(ply, cmd, args)
-    if ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() then
-        SetGlobalBool("ixRationOnline", true)
-        PlayTimedEventSound(4, "ambient/alarms/warningbell1.wav")
-        PlayTimedEventSound(6, "ambient/alarms/warningbell1.wav")
-        PlayTimedEventSound(8, "ambient/alarms/warningbell1.wav")
-
-
-        local sounds = {"npc/overwatch/radiovoice/on3.wav", "npc/overwatch/radiovoice/attention.wav", "npc/overwatch/radiovoice/allunitsat.wav", "npc/overwatch/radiovoice/distributionblock.wav", "npc/overwatch/radiovoice/respond.wav", "npc/overwatch/radiovoice/off2.wav"}
-
-        for _, v in ipairs(player.GetAll()) do
-            if v:IsCombine() then
-                ix.util.EmitQueuedSounds(v, sounds, 0, 0.2, 100)
-            end
         end
+    }
+)
 
-        ix.chat.Send(ply, "dispatchradio", "Attention Units, Ration Protocol is now active, Respond.")
-    end
-end)
+concommand.Add(
+    "ix_curfew",
+    function(ply, cmd, args, client)
+        if ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() then
+            SetGlobalBool("ixRationOnline", false)
+            PlayTimedEventSound(4, "npc/overwatch/cityvoice/f_trainstation_assemble_spkr.wav")
+            local sounds = {"npc/overwatch/radiovoice/on3.wav", "npc/overwatch/radiovoice/attention.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/search.wav", "npc/overwatch/radiovoice/inprogress.wav", "npc/overwatch/radiovoice/_comma.wav", "npc/overwatch/radiovoice/preparetoinnoculate.wav", "npc/overwatch/radiovoice/off2.wav"}
+            for _, v in ipairs(player.GetAll()) do
+                if v:IsCombine() then
+                    ix.util.EmitQueuedSounds(v, sounds, 0, 0.2, 100)
+                end
+            end
 
-concommand.Add("ix_closerations", function(ply, cmd, args)
-    if ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() then
-        SetGlobalBool("ixRationOnline", false)
-        ix.chat.Send(ply, "adminchat", "Rations Closed", true)
+            ix.chat.Send(ply, "dispatchradio", "Attention, city-wide block search in progress. All local Protection Team units: prepare to inoculate.")
+            timer.Simple(
+                4,
+                function()
+                    ix.chat.Send(ply, "dispatch", "Citizen notice: priority identification check in progress. Please assemble in your designated inspection positions.")
+                end
+            )
+        end
     end
-end)
+)
+
+concommand.Add(
+    "ix_rations",
+    function(ply, cmd, args)
+        if ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() then
+            SetGlobalBool("ixRationOnline", true)
+            PlayTimedEventSound(4, "ambient/alarms/warningbell1.wav")
+            PlayTimedEventSound(6, "ambient/alarms/warningbell1.wav")
+            PlayTimedEventSound(8, "ambient/alarms/warningbell1.wav")
+            local sounds = {"npc/overwatch/radiovoice/on3.wav", "npc/overwatch/radiovoice/attention.wav", "npc/overwatch/radiovoice/allunitsat.wav", "npc/overwatch/radiovoice/distributionblock.wav", "npc/overwatch/radiovoice/respond.wav", "npc/overwatch/radiovoice/off2.wav"}
+            for _, v in ipairs(player.GetAll()) do
+                if v:IsCombine() then
+                    ix.util.EmitQueuedSounds(v, sounds, 0, 0.2, 100)
+                end
+            end
+
+            ix.chat.Send(ply, "dispatchradio", "Attention Units, Ration Protocol is now active, Respond.")
+        end
+    end
+)
+
+concommand.Add(
+    "ix_closerations",
+    function(ply, cmd, args)
+        if ply:IsSuperAdmin() or ply:IsAdmin() or ply:IsGamemaster() then
+            SetGlobalBool("ixRationOnline", false)
+            ix.chat.Send(ply, "adminchat", "Rations Closed", true)
+        end
+    end
+)
